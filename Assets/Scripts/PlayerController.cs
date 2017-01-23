ec24d8009c306ccbf7c000e5a8fdc6093a79fbe5
TrangD <trang.d.wiredelta@gmail.com>
2017-01-23 17:20:30 +0100

    add counter, finished

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public Text countText;
+	public Text winText;
+
 	private Rigidbody rb;
+	private int count;
 
 	void Start(){
 		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
 
 	void FixedUpdate () {
@@ -19,4 +27,22 @@ public class PlayerController : MonoBehaviour {
 		rb.AddForce (movement * speed);
 	}
 
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.CompareTag("PickUp"))
+		{
+			other.gameObject.SetActive (false);
+			count += 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12)
+		{
+			winText.text = "You Win!";
+		}
+	}
 }
